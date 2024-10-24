85d3994cdf1876a25e018ca8e4b9c841f435ddc3
Idlir <idlirbregu02@gmail.com>
2024-10-24 10:23:17 +0200

    updates Signup

--- app/src/main/java/com/example/nicestart/Signup.java
+++ app/src/main/java/com/example/nicestart/Signup.java
@@ -1,14 +1,19 @@
 package com.example.nicestart;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Signup extends AppCompatActivity {
 
@@ -22,16 +27,21 @@ public class Signup extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageView bHip = findViewById(R.id.hip);
+
+        Glide.with(this)
+                .load(R.drawable.hipman)
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_700)))
+                .into(bHip);
+
     }
     public void openMain(View v) {
         Intent intent = new Intent(Signup.this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-
-    public void openLogin(View v) {
-        Intent intent = new Intent(Signup.this, Login.class);
         startActivity(intent);
     }
 
