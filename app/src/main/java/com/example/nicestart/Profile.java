9131760db64d0dc3c1409224841ea4f53620f62e
Idlir <idlirbregu02@gmail.com>
2024-10-24 12:24:21 +0200

    added new activity_profile.xml with previous main content (now empty)

--- app/src/main/java/com/example/nicestart/Profile.java
+++ app/src/main/java/com/example/nicestart/Profile.java
@@ -2,11 +2,7 @@ package com.example.nicestart;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,20 +13,28 @@ import androidx.core.view.WindowInsetsCompat;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        ImageView bFinger = findViewById(R.id.profile);
 
+        Glide.with(this)
+                .load("https://cdn-icons-png.flaticon.com/512/25/25231.png")
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                //.centerCrop()
+                .circleCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+                .into(bFinger);
 
     }
 }
